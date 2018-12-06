cf36237d49a73f4290d43479f2b9164ca2dfbaa8
ig09468 <jeremy.meyna@gmail.com>
2018-12-06 12:20:13 +0100

    Préparation du fichier calcul.pl

--- src/calcul.pl
+++ src/calcul.pl
@@ -1,12 +1,36 @@
 /* station(Station, LLignes) qui retourne vrai si la liste LLignes contient toutes les lignes passant par la Station */
-/*station(_,[]). */
-/*station(STATION, [X|QLLIGNES]):- */
-/*	num_station(STATION, X, _,_,_,_), */
-/*	station(STATION, QLLIGNES). */
+station(STATION, LLIGNES):-
+	num_station(STATION, LLIGNES, _,_,_,_).
+
+/* compare_ligne(LLIGNE1, LLIGNE2, LSTATIONS) compare la liste des stations des deux lignes et retourne la liste */
+compare_ligne([],[],_).
+compare_ligne([STATION|NEXT],[STATION|NEXT2], [STATION|LSTATIONS]):-
+	compare_ligne(NEXT,NEXT2,LSTATIONS).
+compare_ligne([STATION1|NEXT],[STATION2|NEXT2],LSTATIONS):-
+	compare_ligne(NEXT,NEXT2,LSTATIONS).
+	
 
 
 /* intersection(LIGNE1, LIGNE2, LSTATIONS) retourne vrai si LSTATIONS contient toutes les stations où LIGNE1 et LIGNE2 se croisent */
-intersection(LIGNE1,LIGNE2):-
-	num_station(X,LIGNE1,_,_,_,_),
-	num_station(X,LIGNE2,_,_,_,_).
+intersection(LIGNE1,LIGNE2, LSTATIONS):-
+	listArrets(LIGNE1, LISTE1),
+	listArrets(LIGNE2, LISTE2),
+	compare_ligne(LISTE1,LISTE2,LSTATIONS).
 
+
+/* correspondance(Ligne, LLIGNESSTATIONS) qui retourne vrai si la liste des stations contient toutes les correspondances possible sur la ligne */
+
+
+
+
+/* dessert(LIGNE, DEPART, ARRIVEE) qui retourne vrai si la ligne dessert les arrêts DEPART et ARRIVEE */
+
+
+
+
+/* nbarret(LIGNE, DEPART, ARRIVEE, DIR, NBARRET) qui calcul dans la variable NBARRET le nombre d'arrêt entre la station DEPART ET ARRIVEE */
+
+nbarret(LIGNE, DEPART, ARRIVEE, DIR, NBARRET):-
+	num_station(DEPART,LIGNE, DIR, NBDIR,_,_),
+	num_station(ARRIVEE,LIGNE,DIR, NBDIR2,_,_),
+	NBARRET is NBDIR-NBDIR2.
